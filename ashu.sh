ffba805ac3b24f292262c2c67fe4697f2fe39f02
araj18oct <araj18oct@gmail.com>
2024-07-31 21:05:32 +0530

    Update ashu.sh

--- ashu.sh
+++ ashu.sh
@@ -1,2 +1,7 @@
-#! /bin/bash
-touch hello_world.sh
+#!/bin/bash
+
+# Create a new file named hello.txt and write "Hello" into it
+echo "Hello" > hello.txt
+
+# Optionally, print a message indicating the file has been created
+echo "File hello.txt created with 'Hello' inside."
